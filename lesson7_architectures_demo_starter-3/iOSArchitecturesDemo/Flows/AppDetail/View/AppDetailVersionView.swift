96a5f0b4c7616948104d7763193a1f9b44dd5cc8
Denis <recmanus@gmail.com>
2021-10-21 14:48:20 +0300

    Lesson7_HomeWork_Step1
    Creating VersionView

--- lesson7_architectures_demo_starter-3/iOSArchitecturesDemo/Flows/AppDetail/View/AppDetailVersionView.swift
+++ lesson7_architectures_demo_starter-3/iOSArchitecturesDemo/Flows/AppDetail/View/AppDetailVersionView.swift
@@ -7,3 +7,42 @@
 //
 
 import UIKit
+
+class AppDetailVersionView: UIView {
+    
+    private(set) lazy var whatsNewLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24.0, weight: .heavy)
+        label.text = "Что нового"
+        label.textColor = .black
+        label.layer.masksToBounds = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private(set) lazy var actualVersionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private(set) lazy var dateOfReleaseLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.boldSystemFont(ofSize: 14.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private(set) lazy var updatesInformationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 14.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+}
